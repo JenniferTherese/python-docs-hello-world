65fcef4cc1a17f6323dec3ba9835984288523b62
JenniferTherese <37617800+JenniferTherese@users.noreply.github.com>
2021-07-14 17:24:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,51 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun 16 14:03:33 2021
-
-@author: Administrator
-"""
-
-from flask import Flask, request
-from flask_restful import Resource, Api
-
-import pandas as pd
-
-details = {
-    'PhoneNo': [9444415512,9176435819,9003282132],
-    'Name' : ['Sam', 'Aishwarya', 'Jen'],
-    'Age' : ['1993-10-12', '1993-09-02', '1992-01-01'],
-    'City' : ['Chennai', 'Bangalore', 'Delhi']
-}
-  
-# creating a Dataframe object 
-df = pd.DataFrame(details, columns = ['PhoneNo','Name', 'Age', 'City'])
-
-
-def get_info(nos):
-    nos = int(nos)
-    # name = df.loc[df['PhoneNo'] == nos, 'Name'].item()
-    # age = df.loc[df['PhoneNo'] == nos, 'Age'].item()
-    # city = df.loc[df['PhoneNo'] == nos, 'City'].item()
-    if nos in df.values :
-        name = df[df['PhoneNo']==nos]['Name'].values[0]
-        age = df[df['PhoneNo']==nos]['Age'].values[0]
-        city = df[df['PhoneNo']==nos]['City'].values[0]
-        result = [name,age,city]
-    else:
-        result = "Validation failed"
-    return result
-
+from flask import Flask
 app = Flask(__name__)
-api = Api(app)
 
-
-
-
-class Information(Resource):
-    def get(self, nos):
-        return {'data': get_info(nos)}
-
-api.add_resource(Information, '/getinfo/<nos>')
+@app.route('/home')
+def hello_world():
+    return 'Hello, World!'
 
 if __name__ == '__main__':
      app.run()
